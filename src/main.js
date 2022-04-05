b22f5a4c3b506c5dd895f4aa068d840331653db7
John-Kim <john@leftium.com>
2022-04-05 13:37:16 +0900

    Enable true HMR.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import './style.css';
 
 import * as PIXI from 'pixi.js';
 
+import { update } from './update.js';
+
 const app = new PIXI.Application({ backgroundColor: 0x1099bb });
 document.body.appendChild(app.view);
 
@@ -11,17 +13,25 @@ const bunny = PIXI.Sprite.from('bunny.png');
 // center the sprite's anchor point
 bunny.anchor.set(0.5);
 
-// move the sprite to the center of the screen
+// move the sprite to random position
 bunny.x = Math.random() * app.screen.width;
 bunny.y = Math.random() * app.screen.height;
 
 app.stage.addChild(bunny);
 
-// Listen for animate update
-app.ticker.add((delta) => {
-    // just for fun, let's rotate mr rabbit a little
-    // delta is 1 if running at 100% performance
-    // creates frame-independent transformation
-    bunny.rotation += 0.1 * delta;
-});
+
+let tickerFunction = (delta) => {
+    update(delta, {bunny});
+}
+app.ticker.add(tickerFunction)
+
+if (import.meta.hot) {
+    import.meta.hot.accept('./update.js', (newUpdate) => {
+        app.ticker.remove(tickerFunction)
+        tickerFunction = (delta) => {
+            newUpdate.update(delta, {bunny});
+        }
+        app.ticker.add(tickerFunction)
+    })
+}
 
---
+++ src/update.js
@@ -0,0 +1,12 @@
+// update.js
+
+function update(delta, {bunny}) {
+    /**/
+    bunny.rotation += 0.1 * delta;
+    /**/
+}
+
+export {
+    update
+}
+
