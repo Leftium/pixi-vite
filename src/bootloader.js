02b84e612c0af826f8ada39f045c2cb6d5a74e39
John-Kim <john@leftium.com>
2022-04-08 16:35:48 +0900

    Convert to custom Pixi game loop.
    https://github.com/pixijs/pixijs/wiki/v5-Custom-Application-GameLoop

--- src/bootloader.js
+++ src/bootloader.js
@@ -5,24 +5,67 @@ import './style.css';
 
 import * as PIXI from 'pixi.js';
 
-import { pixiOptions, init, update, getState, setState } from './main.js';
+import { pixiOptions, init} from './main.js';
 
-const app = new PIXI.Application(pixiOptions);
-document.body.appendChild(app.view);
+
+// based on: https://github.com/pixijs/pixijs/wiki/v5-Custom-Application-GameLoop#custom-gameloop
+// setup renderer and ticker
+var renderer = new PIXI.Renderer(pixiOptions);
+document.body.appendChild(renderer.view);
+var stage = new PIXI.Container();
+
+// We're not using the Pixi application plugin,
+// but we provide interface expected for Pixi app:
+var app = { renderer, stage };
+app.screen = renderer.screen;
+app.ticker = { deltaMS: 0 };
+
+
+// These imported methods must be var (not const) for HMR:
+const NULL_FUNCTION = function() {};
+var update   = NULL_FUNCTION;
+var getState = NULL_FUNCTION;
+var setState = NULL_FUNCTION;
+import('./main.js').then(module => {
+    ({ update, getState } = module);
+});
+
+// setup RAF
+var oldTime = Date.now();
+
+requestAnimationFrame(animate);
+function animate() {
+    var newTime = Date.now();
+    var deltaTime = newTime - oldTime;
+    oldTime = newTime;
+    if (deltaTime < 0) deltaTime = 0;
+    if (deltaTime > 1000) deltaTime = 1000;
+    var deltaFrame = deltaTime * 60 / 1000; //1.0 is for single frame
+
+    app.ticker.deltaMS = deltaTime;
+    // update your game there
+    update(deltaFrame);
+
+    var state = getState()
+    console.log(state?.v);
+
+    renderer.render(stage);
+
+    requestAnimationFrame(animate);
+}
 
 init(app);
 
-app.ticker.add(update);
-let tickerToRemove = update;
 
 if (import.meta.hot) {
     import.meta.hot.accept('./main.js', (newMain) => {
-        app.ticker.remove(tickerToRemove);
 
-        newMain.setState(getState());
+        // Use old method to get state before updating methods.
+        var state = getState();
+        console.log(state?.v);
 
-        app.ticker.add(newMain.update);
-        tickerToRemove = newMain.update;
+        ({ update, setState, getState } = newMain);
+        newMain.setState(state);
+
     })
 }
-
